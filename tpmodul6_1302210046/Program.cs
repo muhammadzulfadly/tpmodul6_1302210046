613af87b85143032b3c7a296f20a7b60f6332191
muhammadzulfadly <muhammadzulfadly@student.telkomuniversity.ac.id>
2023-03-27 05:08:53 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302210046/Program.cs
+++ tpmodul6_1302210046/Program.cs
@@ -1,3 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
 
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title) {
+        this.id = new Random().Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count) {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails() {
+        Console.WriteLine("id:" + id + "title:" + title + "playCount:" + playCount);
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Muhammad Zulfadly");
+        video.PrintVideoDetails();
+    }
+}
