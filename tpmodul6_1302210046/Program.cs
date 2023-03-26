0e528dbfb48727e519adbe81df65442f30362c9e
muhammadzulfadly <muhammadzulfadly@student.telkomuniversity.ac.id>
2023-03-27 05:44:40 +0700

    implementasi design by contract

--- tpmodul6_1302210046/Program.cs
+++ tpmodul6_1302210046/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Diagnostics.Contracts;
 
 public class SayaTubeVideo
 {
@@ -8,23 +9,41 @@ public class SayaTubeVideo
     private int playCount;
 
     public SayaTubeVideo(string title) {
+        Contract.Requires(title != null && title.Length <= 100);
+        Contract.Requires(playCount <= 10000000);
+
         this.id = new Random().Next(10000, 99999);
         this.title = title;
         this.playCount = 0;
     }
 
     public void IncreasePlayCount(int count) {
-        this.playCount += count;
+        try {
+            checked {
+                this.playCount += count;
+            }
+        }
+        catch (OverflowException check) {
+            Console.WriteLine(check.Message);
+        }
     }
 
     public void PrintVideoDetails() {
-        Console.WriteLine("id:" + id + "title:" + title + "playCount:" + playCount);
+        Console.WriteLine("ID: " + id + "\nTitle: " + title + "\nplayCount: " + playCount);
     }
 }
 
 class Program {
     static void Main(string[] args) {
+
         SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Muhammad Zulfadly");
+        video.IncreasePlayCount(500);
+        video.PrintVideoDetails();
+        Console.WriteLine("");
+
+        for(int i = 1; i < 10000000; i++) {
+            video.IncreasePlayCount(1);
+        }
         video.PrintVideoDetails();
     }
 }
